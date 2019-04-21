43e9b0dfd9ffa2720dce8bd6cb531efe9bd50402
Clayton Stone <devcircus@outlook.com>
2019-04-21 09:57:22 -0500

    Tweak pagination view.

--- resources/js/Shared/Pagination.vue
+++ resources/js/Shared/Pagination.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="mt-6 -mb-1 flex flex-wrap">
         <template v-for="(link, key) in links">
-            <div v-if="link.url === null" :key="key" class="mr-1 mb-1 px-4 py-3 text-sm border rounded text-gray" :class="{ 'ml-auto bg-blue-500 text-white': link.label === 'Next', 'bg-blue-500 text-white': link.label === 'Previous' }">{{ link.label }}</div>
-            <inertia-link v-else :key="key" replace class="mr-1 mb-1 px-4 py-3 text-sm border rounded hover:bg-white focus:border-indigo-500 focus:text-indigo-500" :class="{ 'bg-white': link.active, 'ml-auto bg-blue-500 text-white': link.label === 'Next', 'bg-blue-500 text-white': link.label === 'Previous' }" :href="link.url">{{ link.label }}</inertia-link>
+            <div v-if="link.url === null" :key="key" class="mr-1 mb-1 px-4 py-3 text-sm font-semibold border-2 border-white rounded text-gray-800" :class="{ 'ml-auto': link.label === 'Next' }">{{ link.label }}</div>
+            <inertia-link v-else :key="key" replace class="mr-1 mb-1 px-4 py-3 text-sm font-semibold border-2 border-white rounded hover:bg-white hover:text-gray-800 focus:border-indigo-500 focus:text-indigo-500" :class="{ 'bg-white': link.active, 'ml-auto bg-blue-500 text-white': link.label === 'Next', 'bg-blue-500 text-white': link.label === 'Previous' }" :href="link.url">{{ link.label }}</inertia-link>
         </template>
     </div>
 </template>
